96c329452533756d24daa8a66c10d0546af2c1fd
Dylan Nelson <44685561+dylannelson@users.noreply.github.com>
2020-12-01 23:52:45 -0800

    addded some data to buttons

--- Drafting/Dashboard/index.js
+++ Drafting/Dashboard/index.js
@@ -1,3 +1,9 @@
+var num_toggled = 0;
+var all_keys;
+var current_toggled = new Set([])
+var names_of_fish = ""
+
+
 //Useful links:
 //  Object.keys() 
 //      - https://stackoverflow.com/questions/26025433/firefox-keys-is-not-a-function
@@ -8,34 +14,49 @@
 //  Setting onClick being inconsistent
 //      -https://stackoverflow.com/questions/15097315/change-onclick-attribute-with-javascript/23412811
 function load_images(){
-    all_buttons = document.getElementsByClassName("toggle");
     all_keys = Object.keys(data)
     for (i =0; i <80; i++){
-        // fish_link = data[all_keys[i]]['icon_url']
-        // all_buttons[i].innerHTML = `<img src="${fish_link}" alt="">`
-        
+        // creates the button to place the image into
         var newButton = document.createElement("button");
         newButton.className = 'toggleON';
-        newButton.id = `fish${i+1}`
+        newButton.id = `fish${(i+1).toString().padStart(2, '0')}`
         newButton.setAttribute('onclick',"toggleButton(this);");
         
-        var innerImage = document.createElement("img");
+        // places the image in the button
+        var innerImage = document.createElement("img"); 
         innerImage.className = "fish";
-        innerImage.src = data[all_keys[i]]['icon_url']
+        innerImage.src = data[all_keys[i]]['icon_url'] // sets the image source to the correct fish
         newButton.appendChild(innerImage);
+        
+        // places the button in the right row
         document.getElementById(`row${i%5}`).appendChild(newButton);
-
     }
 }
-
 //Useful Links
 //  self referring onlcick
 //      -https://stackoverflow.com/questions/619514/javascript-how-to-make-a-control-send-itself-in-a-method
 function toggleButton(givenCreature){
+    fish_id = Number((givenCreature.id).slice(-2));
+    fish_name = all_keys[fish_id-1];
     if (givenCreature.className == 'toggleON'){
         givenCreature.className = 'toggleOFF';
+        num_toggled += 1;
+        current_toggled.add(fish_name);
     }
     else{
         givenCreature.className = 'toggleON';
+        num_toggled -= 1;
+        current_toggled.delete(fish_name);
     }
+    document.getElementById('num_toggled').innerHTML = `Total Missing Fish: ${num_toggled}`;
+    
+    // making a string of all fish names
+    var curr_names = Array.from(current_toggled)
+    names_of_fish = ''
+    for (i = 0; i < current_toggled.size; i++){
+        names_of_fish += curr_names[i] + ', ';
+    }
+    names_of_fish = names_of_fish.slice(0, -2)
+
+    document.getElementById('name_toggled').innerHTML = `Names of Missing Fish: ${names_of_fish}`;
 }
--- Drafting/README.md
+++ Drafting/README.md
@@ -4,4 +4,10 @@ This is where we hope to show the development of our project, showing our proces
 ### 11/27/2020  
 ![nov27th progress](Sketches/11-27.gif)
 - made a grid of the fish
-- added ability to toggle buttons
+- added ability to toggle buttons  
+  
+### 12/1/2020  
+![dec1st progress](Sketches/12-1.PNG)
+- added data to buttons
+- made collections to store the data
+- (kinda) neatly organizes the data to make it readable
